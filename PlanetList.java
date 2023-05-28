6851e2a5e7c9ce4c34610d7d559c0a24fc4178d4
ilya_k <ivk68@list.ru>
2023-05-28 22:22:30 +0300

    has been resolved

---
+++ PlanetList.java
@@ -0,0 +1,28 @@
+package HW_3;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PlanetList {
+
+    public static void main(String[] args) {
+        List<String> planets = new ArrayList<String>();
+        planets.add("Mercury");
+        planets.add("Venus");
+        planets.add("Earth");
+        planets.add("Mars");
+        planets.add("Jupiter");
+        planets.add("Saturn");
+        planets.add("Uranus");
+        planets.add("Neptune");
+        planets.add("Pluto");
+        planets.add("Venus");
+        planets.add("Earth");
+        planets.add("Mars");
+
+        for (String planet : planets) {
+            int frequency = Collections.frequency(planets, planet);
+            System.out.println(planet + ": " + frequency);
+        }
+    }
+}
